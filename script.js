33e4c58447335861a7fb3dcac77861275b1fc706
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-08-16 15:20:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+	var boldWords = document.querySelectorAll("strong");
+    for (var i = 0; i < boldWords.length; i++) {
+        boldWords[i].style.color = "green";
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	var blodOut= document.querySelectorAll("strong");
+	for(var i=0;i<blodOut.length;i++){
+		blodOut[i].style.color="black";
+	}
     
 }
